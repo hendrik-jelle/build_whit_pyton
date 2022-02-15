61a362b3de8b349148fffc8feceeba675f802b6e
Hendrik Stiemsma (HJ) <Hendrik.stiemsma@politie.nl>
2022-02-15 10:03:34 +0100

    test

--- exp_db.py
+++ exp_db.py
@@ -1,4 +1,22 @@
 #%%
 import sqlite3
+from tkinter import INSERT
 
 #%%
+con= sqlite3.connect("database.db")
+# %%
+cursor = con.cursor()
+# %%
+qry_define_table = '''
+CREATE TABLE training
+(
+    name text,
+    participents real
+)
+
+'''
+# %%
+qry_insert_values = '''
+INSERT INTO training ('python', 10)
+'''
+# %%
